0d6f03e2030dec773b99665bec46f6e706b5d46b
Yimlrt <mangayinileth@gmail.com>
2026-08-19 11:03:09 -0500

    Update and rename taller_14.py to ejericio 5.py

---
+++ ejericio 5.py
@@ -0,0 +1,12 @@
+
+# 4. Ciclo for del 1 al 20, indicando pares e impares
+
+print("=== Punto 4: Números del 1 al 20 ===")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(f"{i} es par")
+    else:
+        print(f"{i} es impar")
+print()
+
+
