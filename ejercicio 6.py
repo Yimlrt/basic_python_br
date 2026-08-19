8845606c7211a96a6a85ab222252b35497e3c96a
Yimlrt <mangayinileth@gmail.com>
2026-08-19 11:04:43 -0500

    Add dictionary for person information

---
+++ ejercicio 6.py
@@ -0,0 +1,15 @@
+
+# 6. Diccionario persona con nombre, edad, carrera y ciudad
+
+print("=== Punto 6: Diccionario persona ===")
+
+persona = {
+    "nombre": "Yimileth",
+    "edad": 21,
+    "carrera": "Ingeniería de Sistemas",
+    "ciudad": "Barranquilla"
+}
+
+print("Información de la persona:")
+print(persona)
+print()
