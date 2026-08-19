d196214a08fee633b5ef8cef7468b8477df496e0
Yimlrt <mangayinileth@gmail.com>
2026-08-19 11:06:58 -0500

    Add ejercicio 10.py file

---
+++ ejercicio 10.py
@@ -0,0 +1,10 @@
+
+# 10. Lista del 1 al 10: lista completa, primer y último elemento,y cantidad de elementos
+print("=== Punto 10: Lista del 1 al 10 ===")
+
+numeros = list(range(1, 11))
+
+print(f"Lista completa: {numeros}")
+print(f"Primer elemento: {numeros[0]}")
+print(f"Último elemento: {numeros[-1]}")
+print(f"Cantidad de elementos: {len(numeros)}")
