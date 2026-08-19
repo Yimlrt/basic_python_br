62a1828e46459fe8b5e5f5ea477a6949623e52c8
Yimlrt <mangayinileth@gmail.com>
2026-08-19 11:01:27 -0500

    Create ejericio 1.py

---
+++ ejericio 1.py
@@ -0,0 +1,10 @@
+# 1. Crear variables curso, semestre, nombre
+curso = "Introducción a Python"
+semestre = "2026-2"
+nombre = "Yimileth"  
+
+print("=== Punto 1: Variables básicas ===")
+print(f"Curso: {curso}")
+print(f"Semestre: {semestre}")
+print(f"Nombre: {nombre}")
+print()
